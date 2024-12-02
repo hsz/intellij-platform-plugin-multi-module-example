09c68473f69fd4706fce600d6286b206c05ac1e7
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2024-12-02 10:43:22 +0100

    Move Gradle repositories configuration to `settings.gradle.kts`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,23 @@
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.1.0"
 }
 
 rootProject.name = "intellij-platform-plugin-multi-module-example"
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    // Configure all project's dependencies
+    repositories {
+        mavenCentral()
+
+        // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+        intellijPlatform {
+            defaultRepositories()
+        }
+    }
+}
